f2f9eda216ed8599c8121d4ac28fb30f9532cb8f
Guido K.B.W. Üffing <gueff@users.noreply.github.com>
2016-01-03 17:46:07 +0100

    Initial commit of myMVC - Hello World ;-)

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @package myMVC
+ * @copyright ueffing.net
+ * @author Guido K.B.W. Üffing <info@ueffing.net>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3. See application/doc/COPYING
+ */
+// bootstrap
+require_once '../application/config/util/bootstrap.php';
+
+// run
+$oMVCApplication = new \MVC\Application($aConfig);
