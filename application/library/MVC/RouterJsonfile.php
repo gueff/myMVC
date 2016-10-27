14d2a0232e5066c5c5b7fb4baf521f6debb5cb62
Guido K.B.W. Üffing <gueff@users.noreply.github.com>
2016-10-27 16:01:34 +0200

    enabling SEO Url Wildcard (/*) Match
    this enables possibility to add wildcard routes to routing.json, so that `/settings/` could become `/settings/*` (note the * at the end).
    
    For example, the following request will point to same routing match (which is: `/settings/*`): 
    
    - /settings/foo/bar/
    
    you could then detect in your controller class what is appending to the proper route:
    
    // gives `foo/bar/`
    $sAppend = mb_substr(
        \MVC\Request::getInstance()->getRequestUri(),
        mb_strlen(str_replace('*', '', \MVC\Registry::get('MVC_ROUTING_CURRENT')['index'])),
        mb_strlen(\MVC\Request::getInstance()->getRequestUri())
    );
    
    
    Example extract of routing.json
    
    [..]
      "/settings/*": {
        "query": "module=standard&c=index&m=settings",
        "title": "Settings",
        "template": {
          "layout": "layout/abk.tpl",
          "contentVar": "sContent",
          "var": {
            "load": {
              "sHead": "layout/header.tpl",
              "sFooter": "layout/footer.tpl",
              "sMainmenu": "",
              "sAsideMenu": "parts/en/asideMenu.tpl",
              "sContent": "content/en/settings.tpl"
            }
          },
          "style": [
          ],
          "script": [
          ]
        },
        "ssl": true
      },
    [..]

--- application/library/MVC/RouterJsonfile.php
+++ application/library/MVC/RouterJsonfile.php
@@ -16,101 +16,150 @@ namespace MVC;
 
 /**
  * RouterJsonfile
- * 
+ *
  * @extends \MVC\RouterJson
  */
 class RouterJsonfile extends \MVC\RouterJson
 {
 
-	/**
-	 * reads the routing.json file and looks for matching routes<br /> 
-	 * The Get-Param `a` will be passed through in both cases
-	 * 
-	 * @access public
-	 * @return void
-	 */
-	public function __construct ()
-	{
-		parent::__construct ();
+    /**
+     * reads the routing.json file and looks for matching routes<br />
+     * The Get-Param `a` will be passed through in both cases
+     *
+     * @access public
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
 
-		if (is_array ($this->_aRouting) && array_key_exists ('QUERY_STRING', $_SERVER))
-		{
-			// right found (GET Params String)
-			// means a request as e.g.
-			//		http://dev.mvc.de/?module=custom&c=index&m=index
-			foreach ($this->_aRouting as $sKey => $aValue)
-			{
-				// if there is no route sepcified in the routing.json (empty), take the MVC fallback routing
-				if (!array_key_exists ('query', $aValue) || $aValue['query'] === '')
-				{
-					$aValue['query'] = Registry::get ('MVC_ROUTING_FALLBACK');
-					// add Target Class
-					parse_str ($aValue['query'], $sQuery);
-					$this->_aRouting[$this->_sRequestUri]['class'] = '\\' . ucfirst ($sQuery[Registry::get ('MVC_GET_PARAM_MODULE')]) . '\\Controller\\' . ucfirst ($sQuery[Registry::get ('MVC_GET_PARAM_C')]);
-				}
+        if (is_array($this->_aRouting) && array_key_exists('QUERY_STRING', $_SERVER))
+        {
+            // right found (GET Params String)
+            // means a request as e.g.
+            //		http://dev.mvc.de/?module=custom&c=index&m=index
+            foreach ($this->_aRouting as $sKey => $aValue)
+            {
+                // if there is no route sepcified in the routing.json (empty), take the MVC fallback routing
+                if (!array_key_exists('query', $aValue) || $aValue['query'] === '')
+                {
+                    $aValue['query'] = Registry::get('MVC_ROUTING_FALLBACK');
+                    // add Target Class
+                    parse_str($aValue['query'], $sQuery);
+                    $this->_aRouting[$this->_sRequestUri]['class'] = '\\' . ucfirst($sQuery[Registry::get('MVC_GET_PARAM_MODULE')]) . '\\Controller\\' . ucfirst($sQuery[Registry::get('MVC_GET_PARAM_C')]);
+                }
 
-				DETECT_APPENDINGS:
-				{
+                DETECT_APPENDINGS:
+                {
+                    $sAppend = '';
+                    $sAppend = trim(substr($_SERVER['QUERY_STRING'], strlen($aValue['query'])));
 
-					$sAppend = '';
-					$sAppend = trim (substr ($_SERVER['QUERY_STRING'], strlen ($aValue['query'])));
+                    // if query string contains the fallback string, cut it out
+                    if (substr($sAppend, 0, strlen(Registry::get('MVC_ROUTING_FALLBACK'))) == Registry::get('MVC_ROUTING_FALLBACK'))
+                    {
+                        $sAppend = substr($sAppend, (strlen(Registry::get('MVC_ROUTING_FALLBACK'))));
+                    }
 
-					// if query string contains the fallback string, cut it out
-					if (substr ($sAppend, 0, strlen (Registry::get ('MVC_ROUTING_FALLBACK'))) == Registry::get ('MVC_ROUTING_FALLBACK'))
-					{
-						$sAppend = substr ($sAppend, (strlen (Registry::get ('MVC_ROUTING_FALLBACK'))));
-					}
+                    $sAppend = '?' . trim(substr($sAppend, 1, strlen($sAppend)));
+                    ($sAppend === '?') ? $sAppend = '' : FALSE;
+                }
 
-					$sAppend = '?' . trim (substr ($sAppend, 1, strlen ($sAppend)));
-					($sAppend === '?') ? $sAppend = '' : FALSE;
-				}
+                // redirect to the SEO Url, which is $sKey here
+                if ($aValue['query'] === substr($_SERVER['QUERY_STRING'], 0, strlen($aValue['query'])))
+                {
+                    Request::REDIRECT($sKey . $sAppend);
+                }
+            }
+        }
 
-				// redirect to the SEO Url, which is $sKey here
-				if ($aValue['query'] === substr ($_SERVER['QUERY_STRING'], 0, strlen ($aValue['query'])))
-				{
-					Request::REDIRECT ($sKey . $sAppend);
-				}
-			}
-		}
+        // SEO Url 1:1 Match
+        if (array_key_exists($this->_sRequestUri, $this->_aRouting))
+        {
+            $aQueryString = $this->_aRouting[$this->_sRequestUri];
 
-		// left found (SEO Url)
-		if (array_key_exists ($this->_sRequestUri, $this->_aRouting))
-		{
-			$aQueryString = $this->_aRouting[$this->_sRequestUri];
+            // use the MVC fallback routing if none is specified in routing.json. @see config
+            if (empty ($aQueryString['query']))
+            {
+                $aQueryString['query'] = Registry::get('MVC_ROUTING_FALLBACK');
+                Log::WRITE('MVC Fallback: ' . $aQueryString['query']);
+            }
 
-			// use the MVC fallback routing if none is specified in routing.json. @see config
-			if (empty ($aQueryString['query']))
-			{
-				$aQueryString['query'] = Registry::get ('MVC_ROUTING_FALLBACK');
-				Log::WRITE ('MVC Fallback: ' . $aQueryString['query']);
-			}
+            // copy to QUERY_STRING
+            $_SERVER['QUERY_STRING'] = $aQueryString['query'];
 
-			// copy to QUERY_STRING
-			$_SERVER['QUERY_STRING'] = $aQueryString['query'];
+            $aParts = explode('&', $aQueryString['query']);
 
-			$aParts = explode ('&', $aQueryString['query']);
+            // copy to GET
+            foreach ($aParts as $aValue['query'])
+            {
+                $aPiece = explode('=', $aValue['query']);
+                (array_key_exists(1, $aPiece)) ? $_GET[$aPiece[0]] = $aPiece[1] : FALSE;
+                (array_key_exists(Registry::get('MVC_GET_PARAM_MODULE'), $_GET)) ? $_GET[Registry::get('MVC_GET_PARAM_MODULE')] = ucfirst($_GET[Registry::get('MVC_GET_PARAM_MODULE')]) : FALSE;
+                (array_key_exists(Registry::get('MVC_GET_PARAM_C'), $_GET)) ? $_GET[Registry::get('MVC_GET_PARAM_C')] = ucfirst($_GET[Registry::get('MVC_GET_PARAM_C')]) : FALSE;
+            }
 
-			// copy to GET
-			foreach ($aParts as $aValue['query'])
-			{
-				$aPiece = explode ('=', $aValue['query']);
-				(array_key_exists (1, $aPiece)) ? $_GET[$aPiece[0]] = $aPiece[1] : FALSE;
-				(array_key_exists (Registry::get ('MVC_GET_PARAM_MODULE'), $_GET)) ? $_GET[Registry::get ('MVC_GET_PARAM_MODULE')] = ucfirst ($_GET[Registry::get ('MVC_GET_PARAM_MODULE')]) : FALSE;
-				(array_key_exists (Registry::get ('MVC_GET_PARAM_C'), $_GET)) ? $_GET[Registry::get ('MVC_GET_PARAM_C')] = ucfirst ($_GET[Registry::get ('MVC_GET_PARAM_C')]) : FALSE;
-			}
+            Request::getInstance()->saveRequest()->prepareQueryVarsForUsage();
+        } 
+        // SEO Url Wildcard (/*) Match
+        else
+        {
+            foreach ($this->_aRouting as $sIndex => $aValue)
+            {
+                if (substr($sIndex, -1) === '*')
+                {
+                    $sWildcard = substr($sIndex, 0, -1);
 
-			Request::getInstance ()->saveRequest ()->prepareQueryVarsForUsage ();
-		}
-	}
+                    if (substr($_SERVER['REQUEST_URI'], 0, strlen($sWildcard)) === $sWildcard)
+                    {
+                        // e.g.: module=default&c=index&m=action
+                        $aQuery = explode('&', $this->_aRouting[$sIndex]['query']);
 
-	/**
-	 * Destrucor
-	 * 
-	 * @access public
-	 * @return void
-	 */	
-	public function __destruct ()
-	{
-		;
-	}
+                        foreach ($aQuery as $sValue)
+                        {
+                            $aEx = explode('=', $sValue);
+                            $_GET[$aEx[0]] = $aEx[1];
+                        }
+
+                        $this->_aRouting[$sIndex]['index'] = $sIndex;
+                        $this->_sRequestUri = $sIndex;
+                        $this->_addParam();
+
+                        Request::getInstance()->saveRequest()->prepareQueryVarsForUsage();
+
+                        return true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * adds params from query to $_GET
+     */
+    private function _addParam()
+    {
+        $aParseUrl = parse_url($_SERVER['REQUEST_URI']);
+
+        if (array_key_exists('query', $aParseUrl))
+        {
+            parse_str($aParseUrl['query'], $aParseStr);
+
+            foreach ($aParseStr as $sKey => $sValue)
+            {
+                $_GET[$sKey] = $sValue;
+            }
+        }
+    }
+
+    /**
+     * Destructor
+     *
+     * @access public
+     * @return void
+     */
+    public function __destruct()
+    {
+        ;
+    }
 }
