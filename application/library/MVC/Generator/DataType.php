77b8a76faa0ca74cbb6ab2780775b1f4e0916ac5
Guido K.B.W. Üffing <gueff@users.noreply.github.com>
2019-12-04 16:35:57 +0100

    Update DataType.php

--- application/library/MVC/Generator/DataType.php
+++ application/library/MVC/Generator/DataType.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * DataType.php
+ *
+ * @package myMVC
+ * @copyright ueffing.net
+ * @author Guido K.B.W. Üffing <info@ueffing.net>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3. See application/doc/COPYING
+ */
 
 namespace MVC\Generator;
 
