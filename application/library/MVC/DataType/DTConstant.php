563898cf2fe80c91fd9415275f4d5f0f3b5ddd45
Guido K.B.W. Üffing <gueff@users.noreply.github.com>
2019-12-04 16:34:13 +0100

    Update DTConstant.php

--- application/library/MVC/DataType/DTConstant.php
+++ application/library/MVC/DataType/DTConstant.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * DTConstant.php
+ *
+ * @package myMVC
+ * @copyright ueffing.net
+ * @author Guido K.B.W. Üffing <info@ueffing.net>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3. See application/doc/COPYING
+ */
+
 
 /**
  * @name $DataTypeDataType
