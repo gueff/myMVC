112ed554a74dc3e55a87e14d566ea09781b93822
Guido K.B.W. Üffing <gueff@users.noreply.github.com>
2019-12-04 16:31:36 +0100

    Update DTArrayObject.php

--- application/library/MVC/DataType/DTArrayObject.php
+++ application/library/MVC/DataType/DTArrayObject.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * DTArrayObject.php
+ *
+ * @package myMVC
+ * @copyright ueffing.net
+ * @author Guido K.B.W. Üffing <info@ueffing.net>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3. See application/doc/COPYING
+ */
 
 /**
  * @name $MVCDataType
