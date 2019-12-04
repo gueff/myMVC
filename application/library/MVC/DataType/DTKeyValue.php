098abc0017e1174a24a56ee253d5bc3527554c02
Guido K.B.W. Üffing <gueff@users.noreply.github.com>
2019-12-04 16:34:29 +0100

    Update DTKeyValue.php

--- application/library/MVC/DataType/DTKeyValue.php
+++ application/library/MVC/DataType/DTKeyValue.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * DTKeyValue.php
+ *
+ * @package myMVC
+ * @copyright ueffing.net
+ * @author Guido K.B.W. Üffing <info@ueffing.net>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3. See application/doc/COPYING
+ */
 
 /**
  * @name $MVCDataType
@@ -7,37 +15,55 @@ namespace MVC\DataType;
 
 class DTKeyValue
 {
-	const DTHASH = 'dce2b4e83004d4b52cfd03cb97fccde3';
+    const DTHASH = 'be72abf628236331ab7b97775e8e4bb1';
 
-	/**
-	 * @var string
-	 */
-	protected $sKey;
+    /**
+     * @var string
+     */
+    protected $sKey;
 
-	/**
-	 * @var mixed
-	 */
-	protected $sValue;
+    /**
+     * @var mixed
+     */
+    protected $sValue;
 
-	/**
-	 * DTKeyValue constructor.
-	 * @param array $aData
-	 */
-	public function __construct(array $aData = array())
-	{
-		$this->sKey = '';
-		$this->sValue = null;
+    /**
+     * @var mixed
+     */
+    protected $mOptional1;
 
-		foreach ($aData as $sKey => $mValue)
-		{
-			$sMethod = 'set_' . $sKey;
+    /**
+     * @var mixed
+     */
+    protected $mOptional2;
 
-			if (method_exists($this, $sMethod))
-			{
-				$this->$sMethod($mValue);
-			}
-		}
-	}
+    /**
+     * @var mixed
+     */
+    protected $mOptional3;
+
+    /**
+     * DTKeyValue constructor.
+     * @param array $aData
+     */
+    public function __construct(array $aData = array())
+    {
+        $this->sKey = '';
+        $this->sValue = null;
+        $this->mOptional1 = null;
+        $this->mOptional2 = null;
+        $this->mOptional3 = null;
+
+        foreach ($aData as $sKey => $mValue)
+        {
+            $sMethod = 'set_' . $sKey;
+
+            if (method_exists($this, $sMethod))
+            {
+                $this->$sMethod($mValue);
+            }
+        }
+    }
 
     /**
      * @param array $aData
@@ -50,120 +76,201 @@ class DTKeyValue
         return $oObject;
     }
 
-	/**
-	 * @param string $mValue 
-	 * @return $this
-	 */
-	public function set_sKey($mValue)
-	{
-		$this->sKey = $mValue;
+    /**
+     * @param string $mValue
+     * @return $this
+     */
+    public function set_sKey($mValue)
+    {
+        $this->sKey = $mValue;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * @param mixed $mValue 
-	 * @return $this
-	 */
-	public function set_sValue($mValue)
-	{
-		$this->sValue = $mValue;
+    /**
+     * @param mixed $mValue
+     * @return $this
+     */
+    public function set_sValue($mValue)
+    {
+        $this->sValue = $mValue;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function get_sKey()
-	{
-		return $this->sKey;
-	}
+    /**
+     * @param mixed $mValue
+     * @return $this
+     */
+    public function set_mOptional1($mValue)
+    {
+        $this->mOptional1 = $mValue;
 
-	/**
-	 * @return mixed
-	 */
-	public function get_sValue()
-	{
-		return $this->sValue;
-	}
+        return $this;
+    }
 
-	/**
-	 * @return string
-	 */
-	public static function getPropertyName_sKey()
-	{
+    /**
+     * @param mixed $mValue
+     * @return $this
+     */
+    public function set_mOptional2($mValue)
+    {
+        $this->mOptional2 = $mValue;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $mValue
+     * @return $this
+     */
+    public function set_mOptional3($mValue)
+    {
+        $this->mOptional3 = $mValue;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function get_sKey()
+    {
+        return $this->sKey;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function get_sValue()
+    {
+        return $this->sValue;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function get_mOptional1()
+    {
+        return $this->mOptional1;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function get_mOptional2()
+    {
+        return $this->mOptional2;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function get_mOptional3()
+    {
+        return $this->mOptional3;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getPropertyName_sKey()
+    {
         return 'sKey';
-	}
+    }
 
-	/**
-	 * @return string
-	 */
-	public static function getPropertyName_sValue()
-	{
+    /**
+     * @return string
+     */
+    public static function getPropertyName_sValue()
+    {
         return 'sValue';
-	}
+    }
 
-	/**
-	 * @return false|string JSON
-	 */
-	public function __toString()
-	{
+    /**
+     * @return string
+     */
+    public static function getPropertyName_mOptional1()
+    {
+        return 'mOptional1';
+    }
+
+    /**
+     * @return string
+     */
+    public static function getPropertyName_mOptional2()
+    {
+        return 'mOptional2';
+    }
+
+    /**
+     * @return string
+     */
+    public static function getPropertyName_mOptional3()
+    {
+        return 'mOptional3';
+    }
+
+    /**
+     * @return false|string JSON
+     */
+    public function __toString()
+    {
         return $this->getPropertyJson();
-	}
+    }
 
-	/**
-	 * @return false|string
-	 */
-	public function getPropertyJson()
-	{
+    /**
+     * @return false|string
+     */
+    public function getPropertyJson()
+    {
         return json_encode($this->getPropertyArray());
-	}
+    }
 
-	/**
-	 * @return array
-	 */
-	public function getPropertyArray()
-	{
+    /**
+     * @return array
+     */
+    public function getPropertyArray()
+    {
         return get_object_vars($this);
-	}
+    }
 
-	/**
-	 * @return array
-	 * @throws \ReflectionException
-	 */
-	public function getConstantArray()
-	{
-		$oReflectionClass = new \ReflectionClass($this);
-		$aConstant = $oReflectionClass->getConstants();
+    /**
+     * @return array
+     * @throws \ReflectionException
+     */
+    public function getConstantArray()
+    {
+        $oReflectionClass = new \ReflectionClass($this);
+        $aConstant = $oReflectionClass->getConstants();
 
-		return $aConstant;
-	}
+        return $aConstant;
+    }
 
-	/**
-	 * @return $this
-	 */
-	public function flushProperties()
-	{
-		foreach ($this->getPropertyArray() as $sKey => $aValue)
-		{
-			$sMethod = 'set_' . $sKey;
+    /**
+     * @return $this
+     */
+    public function flushProperties()
+    {
+        foreach ($this->getPropertyArray() as $sKey => $aValue)
+        {
+            $sMethod = 'set_' . $sKey;
 
-			if (method_exists($this, $sMethod)) 
-			{
-				$this->$sMethod('');
-			}
-		}
+            if (method_exists($this, $sMethod))
+            {
+                $this->$sMethod('');
+            }
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * @return string JSON
-	 */
-	public function getDataTypeConfigJSON()
-	{
-		return '{"name":"KeyValue","file":"KeyValue.php","extends":"","namespace":"MVC\\\\DataType","constant":[],"property":[{"key":"sKey","var":"string","value":null,"visibility":"protected","static":false,"setter":true,"getter":true,"explicitMethodForValue":false,"listProperty":true,"createStaticPropertyGetter":true,"setValueInConstructor":true},{"key":"sValue","var":"mixed","value":"null","visibility":"protected","static":false,"setter":true,"getter":true,"explicitMethodForValue":false,"listProperty":true,"createStaticPropertyGetter":true,"setValueInConstructor":true}],"createHelperMethods":true}';
-	}
+    /**
+     * @return string JSON
+     */
+    public function getDataTypeConfigJSON()
+    {
+        return '{"name":"KeyValue","file":"KeyValue.php","extends":"","namespace":"MVC\\\\DataType","constant":[],"property":[{"key":"sKey","var":"string","value":null,"visibility":"protected","static":false,"setter":true,"getter":true,"explicitMethodForValue":false,"listProperty":true,"createStaticPropertyGetter":true,"setValueInConstructor":true},{"key":"sValue","var":"mixed","value":"null","visibility":"protected","static":false,"setter":true,"getter":true,"explicitMethodForValue":false,"listProperty":true,"createStaticPropertyGetter":true,"setValueInConstructor":true},{"key":"mOptional1","var":"mixed","value":"null","visibility":"protected","static":false,"setter":true,"getter":true,"explicitMethodForValue":false,"listProperty":true,"createStaticPropertyGetter":true,"setValueInConstructor":true},{"key":"mOptional2","var":"mixed","value":"null","visibility":"protected","static":false,"setter":true,"getter":true,"explicitMethodForValue":false,"listProperty":true,"createStaticPropertyGetter":true,"setValueInConstructor":true},{"key":"mOptional3","var":"mixed","value":"null","visibility":"protected","static":false,"setter":true,"getter":true,"explicitMethodForValue":false,"listProperty":true,"createStaticPropertyGetter":true,"setValueInConstructor":true}],"createHelperMethods":true}';
+    }
 
 }
