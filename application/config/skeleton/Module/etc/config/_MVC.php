99fd3c783d93dac38ccc9693306a76128da1c07d
Guido K.B.W. Üffing <gueff@users.noreply.github.com>
2019-12-15 23:24:37 +0100

    skeleton changes

--- application/config/skeleton/Module/etc/config/_MVC.php
+++ application/config/skeleton/Module/etc/config/_MVC.php
@@ -3,12 +3,6 @@
 //-------------------------------------------------------------------------------------
 // MVC
 
-// override MVC's request "a"
-$aConfig['MVC_REQUEST_WHITELIST_PARAMS']['GET']['a'] = array(
-    'regex' => "/[^\\p{L}\\p{M}\\p{Z}\\p{S}\\p{N}\\p{Pd}\\p{Pc},;.:_\\ ?!&\|\{\}\"']+/u"
-, 'length' => 256
-);
-
 // enable debug output
 $aConfig['MVC_DEBUG'] = false;
 
@@ -20,14 +14,15 @@ $aConfig['MVC_ROUTING_FALLBACK'] = $aConfig['MVC_GET_PARAM_MODULE'] . '={module}
     . $aConfig['MVC_GET_PARAM_C'] . '=index&'
     . $aConfig['MVC_GET_PARAM_M'] . '=index';
 
-// extra composer dir
+// composer dir
 $aConfig['MVC_COMPOSER_DIR'] = realpath(__DIR__ . '/../') . '/config/{module}';
 
 // routing.json file
 $aConfig['MVC_ROUTING_JSON'] = realpath(__DIR__ . '/../') . '/routing/final.json';
 
 // Smarty
-$aConfig['MVC_SMARTY_TEMPLATE_DEFAULT'] = 'layout/index.tpl';
+$aConfig['MVC_VIEW_TEMPLATES'] = $aConfig['MVC_MODULES'] . '/' . basename(realpath(__DIR__ . '/../../')) . '/templates';
+$aConfig['MVC_SMARTY_TEMPLATE_DEFAULT'] = 'Frontend/layout/index.tpl';
 
 // Add Location of Smarty PlugIns
 $aConfig['MVC_SMARTY_PLUGINS_DIR'][] = $aConfig['MVC_MODULES'] . '/' . basename(realpath(__DIR__ . '/../../')) . '/etc/smarty';
