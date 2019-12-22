384c5c0b03139e048432f05554bc1a6cd792aa86
Guido K.B.W. Üffing <gueff@users.noreply.github.com>
2019-12-22 19:28:16 +0100

    sendSecurityHeader

--- application/config/skeleton/Module/View/Index.php
+++ application/config/skeleton/Module/View/Index.php
@@ -24,14 +24,30 @@ class Index extends \MVC\View
 {
     /**
      * Index constructor.
-     * @throws ReflectionException
+     * @throws \ReflectionException
      */
 	public function __construct ()
 	{
 		parent::__construct ();
 
+        $this->sendSecurityHeader();
+
 		$this->sContentVar = 'sContent';
 	}
+
+    /**
+     * set HTTP Security Header
+     * @throws \ReflectionException
+     */
+    public function sendSecurityHeader()
+    {
+        header("Content-Security-Policy: " . \MVC\Registry::get('CONTENT_SECURITY_POLICY')['Content-Security-Policy']);         // Default
+        header("X-Content-Security-Policy: " . \MVC\Registry::get('CONTENT_SECURITY_POLICY')['Content-Security-Policy']);       // IE
+        header("X-Webkit-CSP: " . \MVC\Registry::get('CONTENT_SECURITY_POLICY')['Content-Security-Policy']);                    // Chrome und Safari
+        header("X-Frame-Options: allow-from " . \MVC\Registry::get('CONTENT_SECURITY_POLICY')['X-Frame-Options']);
+        header("X-XSS-Protection: " . \MVC\Registry::get('CONTENT_SECURITY_POLICY')['X-XSS-Protection']);
+        header("Strict-Transport-Security: " . \MVC\Registry::get('CONTENT_SECURITY_POLICY')['Strict-Transport-Security']);
+    }
 
     /**
      * @param array $aRouting
