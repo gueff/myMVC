628f895493ad87f31b0808fd35ca6e320156df76
Guido K.B.W. Üffing <gueff@users.noreply.github.com>
2019-12-29 12:55:37 +0100

    fallback env develop

--- application/config/skeleton/Module/install.sh
+++ application/config/skeleton/Module/install.sh
@@ -3,12 +3,24 @@
 MODULENAME="$(basename "$(pwd)")";
 #-------------------
 
+unset MVC_ENV;
+sFallbackMvcEnv="develop";
+
+if [ -z ${MVC_ENV+x} ]; then
+    echo "MVC_ENV has not been set yet.";
+    echo "MVC_ENV will now be set to '$sFallbackMvcEnv'.";
+    export MVC_ENV=$sFallbackMvcEnv;
+else
+    echo "MVC_ENV is set to '$MVC_ENV'";
+fi
+
 . publish.sh
 
 MVC_APPLICATION_PATH='../../application';
-
 echo "installing module libraries...";
 php $MVC_APPLICATION_PATH/composer.phar --working-dir=./etc/config/$MODULENAME/ install;
 echo "...done!";
 
-
+echo "installing Blogimus config for: MVC_ENV=$MVC_ENV...";
+cp etc/config/Blogimus/config/develop.example etc/config/Blogimus/config/$MVC_ENV.php
+echo "...done!";
