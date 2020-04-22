af40a4f45b724a145702f813ed1eb6ec884cb387
Guido K.B.W. Üffing <gueff@users.noreply.github.com>
2020-04-22 08:54:18 +0200

    mkdir +3rd param
    - mkdir +3rd param
    - return bool value only if admitted by method param

--- application/library/MVC/Lock.php
+++ application/library/MVC/Lock.php
@@ -1,16 +1,16 @@
 <?php
 /**
  * Lock.php
- *
- * @package myMVC
+ * @package   myMVC
  * @copyright ueffing.net
- * @author Guido K.B.W. Üffing <info@ueffing.net>
- * @license GNU GENERAL PUBLIC LICENSE Version 3. See application/doc/COPYING
+ * @author    Guido K.B.W. Üffing <info@ueffing.net>
+ * @license   GNU GENERAL PUBLIC LICENSE Version 3. See application/doc/COPYING
  */
 
 /**
  * @name $MVC
  */
+
 namespace MVC;
 
 use MVC\DataType\DTArrayObject;
@@ -24,15 +24,19 @@ class Lock
 {
     /**
      * @param string $sKey
-     * @param bool $bReturn
+     * @param bool   $bReturn
      * @return bool
      * @throws \ReflectionException
      */
     public static function create($sKey = '', $bReturn = false)
     {
         $aBacktrace = Helper::PREPAREBACKTRACEARRAY(debug_backtrace());
-        $sCacheDir = (true === Registry::isRegistered('MVC_CACHE_DIR')) ? Registry::get('MVC_CACHE_DIR') : false;
-        (false === file_exists($sCacheDir)) ? $sCacheDir = sys_get_temp_dir() : false;
+        $sCacheDir = (true === Registry::isRegistered('MVC_CACHE_DIR'))
+            ? Registry::get('MVC_CACHE_DIR')
+            : false;
+        (false === file_exists($sCacheDir))
+            ? $sCacheDir = sys_get_temp_dir()
+            : false;
 
         $sPrefix = preg_replace('/[^\\pL\d_]+/u', '-', $sKey);
         $sPrefix = trim($sPrefix, "-");
@@ -40,19 +44,26 @@ class Lock
         $sPrefix = strtolower($sPrefix);
         $sPrefix = preg_replace('/[^-a-z0-9_]+/', '', $sPrefix);
         $sPrefix = substr($sPrefix, 0, 10);
-        $sPrefix = str_pad ( $sPrefix, 10, '_' );
+        $sPrefix = str_pad($sPrefix, 10, '_');
         $sKey = $sPrefix . '.' . str_pad(strlen($sKey), 2, '_', STR_PAD_LEFT) . '.' . md5($sKey) . '.' . md5(serialize($aBacktrace)) . '.lock';
         $sFile = $sCacheDir . '/' . $sKey;
 
         Log::WRITE('Lock: ' . $aBacktrace['sFile'] . ', ' . $aBacktrace['sLine']);
 
-        if (file_exists($sFile) || false === mkdir($sFile, 0700))
+        $oDTArrayObject = DTArrayObject::create()
+            ->add_aKeyValue(DTKeyValue::create()
+                ->set_sKey('aBacktrace')
+                ->set_sValue($aBacktrace))
+            ->add_aKeyValue(DTKeyValue::create()
+                ->set_sKey('sFile')
+                ->set_sValue($sFile));
+
+        if (true === file_exists($sFile) || false === mkdir($sFile, 0700, true))
         {
-            Event::RUN('mvc.lock.create', DTArrayObject::create()
-                ->add_aKeyValue(DTKeyValue::create()->set_sKey('aBacktrace')->set_sValue($aBacktrace))
-                ->add_aKeyValue(DTKeyValue::create()->set_sKey('bLocked')->set_sValue(true))
-                ->add_aKeyValue(DTKeyValue::create()->set_sKey('sFile')->set_sValue($sFile))
-            );
+            $oDTArrayObject->add_aKeyValue(DTKeyValue::create()
+                ->set_sKey('bLocked')
+                ->set_sValue(true));
+            Event::RUN('mvc.lock.create', $oDTArrayObject);
 
             if (true === $bReturn)
             {
@@ -64,12 +75,14 @@ class Lock
 
         \register_shutdown_function('rmdir', $sFile);
 
-        Event::RUN('mvc.lock.create', DTArrayObject::create()
-            ->add_aKeyValue(DTKeyValue::create()->set_sKey('aBacktrace')->set_sValue($aBacktrace))
-            ->add_aKeyValue(DTKeyValue::create()->set_sKey('bLocked')->set_sValue(false))
-            ->add_aKeyValue(DTKeyValue::create()->set_sKey('sFile')->set_sValue($sFile))
-        );
+        $oDTArrayObject->add_aKeyValue(DTKeyValue::create()
+            ->set_sKey('bLocked')
+            ->set_sValue(false));
+        Event::RUN('mvc.lock.create', $oDTArrayObject);
 
-        return true;
+        if (true === $bReturn)
+        {
+            return true;
+        }
     }
 }
