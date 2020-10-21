a1324c5744fb2bc61dd16201699063dc7042eb90
Guido K.B.W. Üffing <gueff@users.noreply.github.com>
2020-10-21 10:35:48 +0200

    Add files via upload

--- application/config/util/functions.php
+++ application/config/util/functions.php
@@ -1,11 +1,23 @@
 <?php
 
+#--------------------------------------------------------------------------------
+#
+
 /**
+ * simplifies the use of variables.
+ * If a variable does not exist, null or a defined value is returned
+ *
+ * // usually
+ * $mValue = (isset($aData['foo']['bar'])) ? $aData['foo']['bar'] : null;
+ * // way easier with get()
+ * $mValue = get($aData['foo']['bar']);
+ *
  * @param      $sVar
  * @param null $mFallback
  * @return mixed|null
- * @example     if (get($_GET['foo']) === 123) {..}
- *              if (get($_GET['foo'], 123) === 123) {..}
+ * @example     if (get($_GET['foo']) === 123) {..}         // value of $_GET['foo'] or null
+ *              if (get($_GET['foo'], 123) === 123) {..}    // value of $_GET['foo'] or 123
+ *              $mValue = get($aData['foo']['bar']);        // value of $aData['foo']['bar'] or null
  */
 function get(&$sVar, $mFallback = null)
 {
@@ -16,3 +28,6 @@ function get(&$sVar, $mFallback = null)
 
     return $mFallback;
 }
+
+#--------------------------------------------------------------------------------
+# wbx_* functions
