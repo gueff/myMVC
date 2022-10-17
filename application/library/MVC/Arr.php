180a7e5a594fe84b5c9a0324a639f71de5d99728
gueff <guido.ueffing@web.de>
2022-10-18 00:43:04 +0200

    update

--- application/library/MVC/Arr.php
+++ application/library/MVC/Arr.php
@@ -4,10 +4,5 @@ namespace MVC;
 
 class Arr
 {
-    /*
-     * @see
-     * /media/admin1/811b9852-6f28-4544-aac6-de1b7658b070/home/admin1/htdocs/_mf/_inkasso/DSGVO_inkasso/libs/MF/Support/Arr.php
-     * /media/admin1/811b9852-6f28-4544-aac6-de1b7658b070/home/admin1/htdocs/_mf/_inkasso/DSGVO_inkasso/libs/MF/Support/helpers.php
-     * /media/admin1/811b9852-6f28-4544-aac6-de1b7658b070/home/admin1/htdocs/_mf/_inkasso/DSGVO_inkasso/libs/MFLib/Util/ObjectConverter.php
-     */
+
 }
