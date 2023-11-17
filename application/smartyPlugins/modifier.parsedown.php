a903e726e51e8643461cec78f085a0664c38d686
gueff <guido.ueffing@web.de>
2023-11-17 16:11:15 +0100

    update

--- application/smartyPlugins/modifier.parsedown.php
+++ application/smartyPlugins/modifier.parsedown.php
@@ -15,5 +15,11 @@ function smarty_modifier_parsedown(string $sMarkdown = '') : string
     $oParsedown = new \Parsedown();
     $sMarkup = $oParsedown->text($sMarkdown);
 
+    $sStart = '<p>';
+    $sEnd = '</p>';
+
+    $sMarkup = (true === str_starts_with($sMarkup, $sStart)) ? substr($sMarkup, strlen($sStart)) : '';
+    $sMarkup = (true === str_ends_with($sMarkup, $sEnd)) ? substr($sMarkup, 0, (strlen($sMarkup) - strlen($sEnd))) : '';
+
     return $sMarkup;
 }
